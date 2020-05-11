01c716fc40e23a613830e4dba519801a5994ba49
vinhthaiduytran <54528745+vinhthaiduytran@users.noreply.github.com>
2020-05-11 05:41:17 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-#!/bin/env python
-from app import create_app, socketio
+from myapp import app
+from myapp import db
 
-app = create_app(debug=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0')
+    db.create_all()
+    app.run(debug=True)
