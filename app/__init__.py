c60a0238201da75cabae06ab9093eb1e86105a91
vinhthaiduytran <54528745+vinhthaiduytran@users.noreply.github.com>
2020-05-11 05:41:42 -0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_heroku import Heroku
-from flask_socketio import SocketIO
 
-socketio = SocketIO()
 heroku = Heroku()
 db = SQLAlchemy()
 login = LoginManager()
@@ -23,13 +21,12 @@ def create_app(debug=False, test_config=None):
         app.config.from_mapping(test_config)
         
     db.init_app(app)
-    socketio.init_app(app)
     login.init_app(app)
     login.login_view = 'login'
+    
     
     with app.app_context():
         # Include our routes
         from . import routes, models
         db.create_all()
         return app
-
